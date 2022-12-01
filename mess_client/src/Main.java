a9e06aeca22850213e52e9afa8eb66a9a9875355
Khoa A. Pham <khoa.a.pham@opswat.com>
2022-12-02 01:07:08 +0700

    First commit

---
+++ mess_client/src/Main.java
@@ -0,0 +1,7 @@
+import Client.LoginUI;
+
+public class Main {
+        public static void main(String[] args) {
+            new LoginUI();
+        }
+}
---
+++ mess_server/src/Main.java
@@ -0,0 +1,7 @@
+import Server.Server;
+
+public class Main {
+    public static void main(String args[]) {
+        new Server();
+    }
+}
